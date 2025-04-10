dc1f3ce851c75c208707344e826f9da46947f064
jdgsd <158793161+jdgsd@users.noreply.github.com>
2025-04-09 23:38:09 -0400

    basemap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -63,7 +63,7 @@
     async function loadMap(){
         map = new mapboxgl.Map({
             container: "map",
-            style: "mapbox://styles/mapbox/light-v11",
+            style: "mapbox://styles/mapbox/streets-v12",
             zoom: 11,
             minZoom: 5,
             maxZoom: 18,
